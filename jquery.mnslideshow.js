a99abff44647abbf9e3778871829f365bc0aa8d2
Markus Nilsson <markus@mnilsson.se>
2010-06-30 19:15:01 +0200

    Added possibility to use fade as transition.

--- jquery.mnslideshow.js
+++ jquery.mnslideshow.js
@@ -1,66 +1,81 @@
 (function($) {
     $.fn.mnslideshow = function(options) {
-        
+
         var defaults = {
-            transition: 'slide',
-            transitionIn: 'slide',
-            transitionOut: 'slide',
+            transition: 'fade',
+            transitionIn: '',
+            transitionOut: '',
             options: {},
-            optionsIn: { direction: 'left' },
-            optionsOut: { direction: 'right' },
+            optionsIn: {},
+            optionsOut: {},
             speed: 1000,
             pause: 2000
         };
-
         var options = $.extend(defaults, options);
-   
         if (options.transitionIn == '') {
             options.transitionIn = options.transition;
         }
         if (options.transitionOut == '') {
             options.transitionOut = options.transition;
         }
-        
         var thisSlide = 0;
         var nextSlide = 1;
         var slides = $(this).find('li');
         $(slides).each(function() {
             $(this).css('display', 'none');
         });
-        
         $(slides[0]).css('display', 'block');
-        
         function animate() {
-            $(slides[thisSlide]).hide(
-                options.transitionOut,
-                options.optionsOut,
-                options.speed
-            );
-            $(slides[nextSlide]).show(
-                options.transitionIn,
-                $.extend(
-                    options.optionsIn,
-                    {
-                        queue: false
+            if( options.transitionOut == 'fade') {
+                $(slides[thisSlide]).fadeOut(
+                    options.speed
+                );
+            } else {
+                $(slides[thisSlide]).hide(
+                    options.transitionOut,
+                    options.optionsOut,
+                    options.speed
+                );
+            }
+            if (options.transitionIn == 'fade') {
+                $(slides[nextSlide]).fadeIn(
+                    options.speed,
+                    function() {
+                        thisSlide = nextSlide;
+                        nextSlide++;
+                        if (slides.length <= nextSlide) {
+                            nextSlide = 0;
+                        }
+                        timeout = setTimeout(function() {
+                            animate();
+                        }, options.pause);
                     }
-                ),
-                options.speed,
-                function() {
-                    thisSlide = nextSlide;
-                    nextSlide++;
-                    if (slides.length <= nextSlide) {
-                        nextSlide = 0;
+                );
+            } else {
+                $(slides[nextSlide]).show(
+                    options.transitionIn,
+                    $.extend(
+                        options.optionsIn,
+                        {
+                            queue: false
+                        }
+                    ),
+                    options.speed,
+                    function() {
+                        thisSlide = nextSlide;
+                        nextSlide++;
+                        if (slides.length <= nextSlide) {
+                            nextSlide = 0;
+                        }
+                        timeout = setTimeout(function() {
+                            animate();
+                        }, options.pause);
                     }
-                    timeout = setTimeout(function() {
-			            animate();
-		            }, options.pause);
-                }
-            );
+                );
+            }
         }
-        
-		timeout = setTimeout(function(){
-			animate();
-		},options.pause);
-	
+        timeout = setTimeout(function(){
+            animate();
+        },options.pause);
     };
 })(jQuery);

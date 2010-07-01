0737779c112db2d221bd9eef4adc13d40061729c
Markus Nilsson <markus@mnilsson.se>
2010-07-01 15:43:04 +0200

    Added example page

---
+++ README.md
@@ -0,0 +1,11 @@
+mnslideshow
+===========
+
+Is a small slideshowplugin for jquery.
+
+Requires jquery and jquery-ui.
+
+The slides can contain anything if it fits in an ul! 
+
+Check example.html for, wait for it, a example!
+    
---
+++ jquery.mnslideshow.js
@@ -0,0 +1,81 @@
+(function($) {
+    $.fn.mnslideshow = function(options) {
+
+        var defaults = {
+            transition: 'fade',
+            transitionIn: '',
+            transitionOut: '',
+            options: {},
+            optionsIn: {},
+            optionsOut: {},
+            speed: 1000,
+            pause: 2000
+        };
+        var options = $.extend(defaults, options);
+        if (options.transitionIn == '') {
+            options.transitionIn = options.transition;
+        }
+        if (options.transitionOut == '') {
+            options.transitionOut = options.transition;
+        }
+        var thisSlide = 0;
+        var nextSlide = 1;
+        var slides = $(this).find('li');
+        $(slides).each(function() {
+            $(this).css('display', 'none');
+        });
+        $(slides[0]).css('display', 'block');
+        function animate() {
+            if( options.transitionOut == 'fade') {
+                $(slides[thisSlide]).fadeOut(
+                    options.speed
+                );
+            } else {
+                $(slides[thisSlide]).hide(
+                    options.transitionOut,
+                    options.optionsOut,
+                    options.speed
+                );
+            }
+            if (options.transitionIn == 'fade') {
+                $(slides[nextSlide]).fadeIn(
+                    options.speed,
+                    function() {
+                        thisSlide = nextSlide;
+                        nextSlide++;
+                        if (slides.length <= nextSlide) {
+                            nextSlide = 0;
+                        }
+                        timeout = setTimeout(function() {
+                            animate();
+                        }, options.pause);
+                    }
+                );
+            } else {
+                $(slides[nextSlide]).show(
+                    options.transitionIn,
+                    $.extend(
+                        options.optionsIn,
+                        {
+                            queue: false
+                        }
+                    ),
+                    options.speed,
+                    function() {
+                        thisSlide = nextSlide;
+                        nextSlide++;
+                        if (slides.length <= nextSlide) {
+                            nextSlide = 0;
+                        }
+                        timeout = setTimeout(function() {
+                            animate();
+                        }, options.pause);
+                    }
+                );
+            }
+        }
+        timeout = setTimeout(function(){
+            animate();
+        },options.pause);
+    };
+})(jQuery);
